31a5c40ce4b9cc2a72e2bf3bd0dc4200550c72e3
Pravin Singh <71260633+Pravin-P-Singh@users.noreply.github.com>
2026-02-19 12:06:24 +0530

    Add Azure provider and resource group configuration

--- terraform/modules/vm/main.tf
+++ terraform/modules/vm/main.tf
@@ -1,1 +1,13 @@
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${var.company_name}-${var.project_name}"
+  location = var.location
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
