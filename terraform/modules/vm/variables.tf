59424fc0711770c283595fb5ca0f30a9b9552d4c
Pravin Singh <71260633+Pravin-P-Singh@users.noreply.github.com>
2026-02-19 12:05:41 +0530

    Add variables for VM configuration in Terraform

--- terraform/modules/vm/variables.tf
+++ terraform/modules/vm/variables.tf
@@ -1,1 +1,8 @@
+variable "company_name" {}
+variable "project_name" {}
+variable "location" {}
+variable "vm_size" {}
+variable "admin_username" {}
+variable "ssh_public_key" {}
+variable "resource_name" {}
 
